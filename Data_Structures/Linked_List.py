e7ea2e4f02adfdc7457a3364fb7965dacc4d1a89
sharif-42 <sharifulcsehstu@gmail.com>
2022-05-16 00:20:26 +0600

    Linked list implemented

--- Data_Structures/Linked_List.py
+++ Data_Structures/Linked_List.py
@@ -1,67 +1,78 @@
+# Create a node
 class Node:
-    def __init__(self, data=None, next_node=None):
+    def __init__(self, data):
         self.data = data
-        self.next_node = next_node
-
-    def get_next_node(self):
-        return self.next_node
-
-    def set_next_node(self, node):
-        self.next_node = node
+        self.next = None
 
 
 class LinkedList:
     def __init__(self):
-        # head None means Linked list is empty
         self.head = None
-        self.size = 0
 
-    def get_size(self):
-        return self.size
+    def insert_at_beginning(self, new_data):
+        """
+        Always Keeping the head at the beginning.
+        :param new_data: data element to be add
+        :return:
+        """
+        new_node = Node(data=new_data)
+        new_node.next = self.head
 
-    # Add a node at the front
-    # head is alway points the first element
-    def insert_at_the_front(self, data):
-        new_node = Node(data, self.head)
         self.head = new_node
-        self.size += 1
 
-    # This function is defined in Linked List class
-    # Appends a new node at the end.  This method is
-    #  defined inside LinkedList class shown above */
+    def print_linked_list(self):
+        print("Element in Linked List")
+        temp_head = self.head
+        while temp_head:
+            print(str(temp_head.data) + " ", end="")
+            temp_head = temp_head.next
+        print()
+
     def insert_at_the_end(self, new_data):
-
-        # 1. Create a new node
-        # 2. Put in the data
-        # 3. Set next as None
-        new_node = Node(new_data)
-
-        # 4. If the Linked List is empty, then make the
-        #    new node as head
-        if self.head is None:
+        new_node = Node(data=new_data)
+        while self.head is None:
             self.head = new_node
             return
-
-        # 5. Else traverse till the last node
         last = self.head
-        while (last.next):
-            last = last.next_node
+        while last.next:
+            last = last.next
+        last.next = new_node
 
-        # 6. Change the next of last node
-        last.next_node = new_node
+    def insert_after(self, prev_node, new_data):
+        """
+        A data will be added after a specific node
+        :param prev_node: Previous node, after that new node will be added
+        :param new_data: the data should be added after the specific node
+        :return: None
+        """
+        new_node = Node(data=new_data)
+        new_node.next = prev_node.next
+        prev_node = new_node.next
 
-    # Utility function to print the linked list
-    def printList(self):
-        temp = self.head
-        while (temp):
-            print(temp.data)
-            temp = temp.next_node
+    def search_element(self, key):
+        """
+        :param key: element should search
+        :return: None
+        """
+        current = self.head
+        while current:
+            if current.data == key:
+                return True
+            current = current.next
+        return False
 
 
-if __name__ == '__main__':
-    lst = LinkedList()
-    lst.insert_at_the_front(10)
-    lst.insert_at_the_front(20)
-    lst.insert_at_the_front(50)
-    print(lst.size)
-    lst.printList()
+if __name__ == "__main__":
+    linked_list = LinkedList()
+    item_to_add = [1, 10, 5, 100, 6, 1000]
+    for item in item_to_add:
+        linked_list.insert_at_beginning(item)
+    linked_list.print_linked_list()
+    linked_list.insert_at_the_end(new_data=2000)
+    linked_list.print_linked_list()
+    key = 5
+    find = linked_list.search_element(key=key)
+    print(f"Key {key} in Linked List?: {find}")
+    linked_list.insert_at_the_end(new_data=22222)
+    linked_list.print_linked_list()
+
